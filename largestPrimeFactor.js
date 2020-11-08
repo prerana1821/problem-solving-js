b6966d579aff27faefcb3699c3f892cc9acb822d
prerana1821 <prerananawar1@gmail.com>
2020-11-09 02:21:34 +0530

    Added Largest Prime Factor

---
+++ largestPrimeFactor.js
@@ -0,0 +1,45 @@
+// num = 13195;
+let count;
+let result = 0;
+let primeFactor = [];
+function largestPrimeFactor(num) {
+    for (let i = 1; i <= num; i++) {
+        count = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            // console.log(i);
+            if (num % i == 0) {
+                // console.log(i);
+                primeFactor.push(i);
+            }
+        }
+    }
+    // console.log(primeFactor.pop());
+    return primeFactor.pop();
+}
+
+console.log(largestPrimeFactor(13195));
+
+// for (let i = 1; i <= num; i++) {
+//     count = 0;
+//     for (let j = 1; j <= i; j++) {
+//         if (i % j == 0) {
+//             count++;
+//         }
+//     }
+//     if (count == 2) {
+//         // console.log(i);
+//         if (num % i == 0) {
+//             // console.log(i);
+//             primeFactor.push(i);
+//         }
+//     }
+// }
+// console.log(primeFactor.pop());
+//OR
+// let largestPrimeFactor = primeFactor[primeFactor.length - 1]
+// console.log(largestPrimeFactor);
