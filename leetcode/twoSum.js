2851f7fc1f6cbd0ebe879a6d48a0574cca3dd79f
prerana1821 <prerananawar1@gmail.com>
2021-01-20 03:07:02 +0530

    Added 2 leetcode problems

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,10 +1,42 @@
-function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        console.log(nums[i], nums[i + 1]);
-        if (nums[i] + nums[i + 1] === target) {
-            return [i, i + 1];
-        }
-    }
-}
+// var twoSum = function(numbers, target) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers.includes(target - numbers[i])) {
+//             return [i, numbers.lastIndexOf(target - numbers[i])];
+//         }
+//     }
+// };
 
-console.log(twoSum([3, 2, 3], 6));
+// var twoSum = function(nums, target) {
+//     var answer = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 answer.push(i);
+//                 answer.push(j);
+//                 // return [i, j];
+//             }
+//         }
+//     }
+//     return answer;
+// };
+
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
+
+var twoSum = function(numbers, target) {
+    const object = {};
+    for (let i = 0; i < numbers.length; i++) {
+        if (object.hasOwnProperty(target - numbers[i])) {
+            return [object[target - numbers[i]], i];
+        }
+        object[numbers[i]] = i;
+    }
+};
+console.log(twoSum([2, 8, 11, 21], 10));
---
+++ leetcode/twoSum2.js
@@ -0,0 +1,54 @@
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i + 1, j + 1];
+//             }
+//         }
+//     }
+// };
+
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i + 1, j + 1];
+//             }
+//         }
+//     }
+// };
+
+// var twoSum = function(numbers, target) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers.includes(target - numbers[i])) {
+//             return [i + 1, numbers.lastIndexOf(target - numbers[i]) + 1];
+//         }
+//     }
+// };
+
+
+var twoSum = function(num, target) {
+    let i = 0;
+    let j = num.length - 1;
+    while (i < j) {
+        if (num[i] + num[j] > target) {
+            j--;
+        } else if (num[i] + num[j] < target) {
+            i++;
+        } else {
+            return [i + 1, j + 1];
+        }
+    }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+
+var twoSum = function(numbers, target) {
+    const object = {};
+    for (let i = 0; i < numbers.length; i++) {
+        if (object.hasOwnProperty(target - numbers[i])) {
+            return [object[target - numbers[i]] + 1, i + 1];
+        }
+        object[numbers[i]] = i;
+    }
+};
